18c8d56368c024acf68efece9127a632bd18b717
HusanboyJorayev <144492116+HusanboyJorayev@users.noreply.github.com>
2024-04-01 17:48:16 +0500

    Initial commit

---
+++ src/main/java/com/example/demo_project_reactive_arxitechture/DemoProjectReactiveArxitechtureApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_project_reactive_arxitechture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectReactiveArxitechtureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProjectReactiveArxitechtureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo_project_reactive_arxitechture/DemoProjectReactiveArxitechtureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_project_reactive_arxitechture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectReactiveArxitechtureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
